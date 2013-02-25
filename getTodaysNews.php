a67400630795d6f6a3ca86df8aa8dff683760e82
Pinak Saha <Pinaksaha90@gmail.com>
2013-02-25 11:53:42 -0500

    santized out put stored imgUrl link display with format

--- getTodaysNews.php
+++ getTodaysNews.php
@@ -1,36 +1,35 @@
 <?php 
 	
-	$dir = './news/';
-	
-	if(is_dir($dir))
-	{	
-		$files = scandir($dir,1);
-	}
-	
-	//print($files[0]);
-
-	$todayDir = $files[0];
-	$dir = $dir.$files[0];
-	//echo($dir);
-	
-	$filesDir = scandir($dir,1);
-	
-	$num = count($filesDir)-2;
-	
-	for($i=0;$i<$num;$i++)
+	function getNews()
 	{
-		$newsFile = $dir."/" . $filesDir[$i];
-		print($newsFile);
-		$handel = fopen($newsFile, "r");
-		$content = fread($handel, filesize($newsFile));
-		$content = unserialize($content);
-		fclose($handel);
-		print "<pre>";
-		print_r($content);
-		print "</pre>";
-	}
-	
-	
+		$dir = './news/';
 		
-	//print_r($files);
+		if(is_dir($dir))
+		{	
+			$files = scandir($dir,1);
+		}
+		
+		//print($files[0]);
+	
+		$todayDir = $files[0];
+		$dir = $dir.$files[0];
+		//echo($dir);
+		
+		$filesDir = scandir($dir,1);
+		$content;
+		$num = count($filesDir)-2;
+		
+		for($i=0;$i<$num;$i++)
+		{
+			$newsFile = $dir."/" . $filesDir[$i];
+			print($newsFile);
+			$handel = fopen($newsFile, "r");
+			$content = fread($handel, filesize($newsFile));
+			$content = unserialize($content);
+			fclose($handel);
+		}
+		
+		return $content;
+	}	
+	
 ?>
