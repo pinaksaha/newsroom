6fcf4b5054fb5df5c1bf78fc1238d8e7eeca69b7
Pinak Saha <Pinaksaha90@gmail.com>
2013-02-24 22:00:59 -0500

    <added getting news and news storage structure>

---
+++ getNews.php
@@ -0,0 +1,56 @@
+<?php
+	$verge = simplexml_load_file('http://www.theverge.com/rss/index.xml');
+    
+    
+    $dir = './news/'.date("m_d_Y").'/';
+    mkdir($dir,0777);
+    $fileName = $dir. date("H_i_s").".json";
+    
+    echo $fileName;
+    
+    $handel = fopen($fileName, 'x+');
+    
+    class Article
+    {
+	    public $__title;
+	    public $__content;
+	    public $__author;
+	    function __construct($title,$content,$author)
+	    
+	    {
+		    $this->__title = $title;
+		    $this->__content = $content;
+		    $this->__author = $author;
+	    }
+    }
+    
+    
+    $img = $json;
+	$json = json_encode($verge);
+	
+	$json = json_decode($json,true);
+	
+	$count = count($json);
+	
+	
+	for($i= 0; $i < $count; $i++)
+	{
+		
+		
+		$author="";
+		
+		for($j=0;$j<count($json[entry][$i][author]);$j++)
+		{
+		 $author = $author ." " .$json[entry][$i][author][$j][name];
+		 print($author);
+		}
+		
+		$news = new Article($json[entry][$i][title],$json[entry][$i][content],$author);
+		$news = serialize($news);
+		
+		fwrite($handel,$news);
+		$author = "";
+	}
+	fclose($handel);
+	var_dump($currentNews);
+?>
